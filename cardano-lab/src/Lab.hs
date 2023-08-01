95deeeea5c414ad81690570efc627b77d4e15e75
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2023-08-01 17:28:15 +0200

    WIP

--- cardano-lab/src/Lab.hs
+++ cardano-lab/src/Lab.hs
@@ -62,17 +62,61 @@ interpretCardanoNodeIO na queue prog =
       cancel asyncHandle
       interpretCardanoNodeIO na queue next
 
-program :: FreeT CardanoNode Mithril ()
-program = do
-  _ <- lift mithrilProgram
-  asyncHandle <- liftF startTheNode
-  liftF $ stopTheNode asyncHandle
+-- * Program
 
-interpretIO :: NodeArguments -> TQueue IO Text -> FreeT CardanoNode Mithril a -> IO a
-interpretIO na queue prog = do
-  r <- interpretMithrilIO $ runFreeT prog
-  case r of
+data ProgramF next 
+  = GetInput (Text -> next)
+  | StopProgram
+  deriving (Functor)
+
+type Program = Free ProgramF
+
+getInput :: Program Text
+getInput = liftF $ GetInput id
+
+stopProgram :: Program a 
+stopProgram = liftF StopProgram
+
+-- program :: Program a
+program = do 
+  i <- getInput
+  case i of
+    "1" -> do 
+       lift mithrilProgram 
+       getInput 
+    -- "2" -> do 
+    --    asyncHandle <- liftF startTheNode 
+    --    undefined
+    "stop" -> stopProgram 
+  
+   
+
+programIO :: NodeArguments -> TQueue IO Text -> Program a -> IO a
+programIO na queue prog = 
+  case runFree prog of
     Pure x -> return x
-    Free a -> do
-      next <- interpretCardanoNodeIO na queue a
-      interpretIO na queue next
+    Free (GetInput next) -> do
+      line <- getLine
+      case line of
+        "1" -> do
+           interpretMithrilIO mithrilProgram
+           programIO na queue $ next "1"
+        "2" -> do 
+          asyncHandle <- interpretCardanoNodeIO na queue startTheNode
+          programIO na queue $ next "2"
+        x -> programIO na queue $ next
+
+-- program :: FreeT CardanoNode Mithril ()
+-- program = do
+--   _ <- lift mithrilProgram
+--   asyncHandle <- liftF startTheNode
+--   liftF $ stopTheNode asyncHandle
+
+-- interpretIO :: NodeArguments -> TQueue IO Text -> FreeT CardanoNode Mithril a -> IO a
+-- interpretIO na queue prog = do
+--   r <- interpretMithrilIO $ runFreeT prog
+--   case r of
+--     Pure x -> return x
+--     Free a -> do
+--       next <- interpretCardanoNodeIO na queue a
+--       interpretIO na queue next
