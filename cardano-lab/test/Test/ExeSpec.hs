a990ecb4eea550b4d819a60bee89f5978c9c7551
Sasha Bogicevic <sasha.bogicevic@iohk.io>
2023-08-13 10:17:10 +0200

    Test that cardano-node and mithril-client executables are in place

--- cardano-lab/test/Test/ExeSpec.hs
+++ cardano-lab/test/Test/ExeSpec.hs
@@ -1,11 +1,13 @@
 module Test.ExeSpec where
 
-import System.Process (callProcess)
+import System.Process (readProcess)
 import Test.Hspec
 import Prelude
 
 spec :: Spec
 spec =
-  describe "cardano-lib executable can be started" $
-    it "should be able to call cardano-lab executable" $
-      callProcess "cabal" ["run", "cardano-lab"] `shouldReturn` ()
+  describe "all external executable are in place" $ do
+    it "should be able to call cardano-node executable" $
+      readProcess "cardano-node" ["--version"] "" >>= (`shouldContain` "8.1.2")
+    it "should be able to call mithril-client executable" $
+      readProcess "mithril-client" ["--version"] "" >>= (`shouldContain` "0.3.20")
